ecf6d2c3b23134e432e1b6bdc426711f17a5e1c5
kps2838 <97150053+kps2838@users.noreply.github.com>
2022-03-31 18:01:04 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(function(){
+    $('#contents .btn > button').click(function(){
+        $('#contents .btn > button').removeClass('on');
+        $(this).addClass('on');
+    });
+
+    $('#contents .btn > button').eq(0).click(function(){
+        $('#contents .news .text-box').removeClass('on');
+        $('#contents .news .notice').addClass('on');
+    });
+
+    $('#contents .btn > button').eq(1).click(function(){
+        $('#contents .news .text-box').removeClass('on');
+        $('#contents .news .community').addClass('on');
+    });
+
+
+    let slideNum = 0;
+    let position = 0;
+
+    $('#gallery .slide-btn .prev').click(function(){
+        if(slideNum > 0){
+            position += 1200;
+            $('.slide-container .images').css('transform', `translateX(${position}px)`)
+            slideNum = slideNum-1;
+        };
+
+        if (slideNum == slideNum){
+            $('#gallery .pagination > div').removeClass('on');
+            $('#gallery .pagination > div').eq(slideNum).addClass('on');
+        };
+
+        if (slideNum == 0){
+            $('.slide-container .images').css('transform', `0`);
+        }
+    });
+
+    $('#gallery .slide-btn .next').click(function(){
+        if(slideNum < 2){
+            position -= 1200;
+            $('.slide-container .images').css('transform', `translateX(${position}px)`)
+            slideNum = slideNum+1;
+        } 
+
+        if (slideNum == slideNum){
+            $('#gallery .pagination > div').removeClass('on');
+            $('#gallery .pagination > div').eq(slideNum).addClass('on');
+        }
+
+        if (slideNum == 2){
+            $('.slide-container .images').css('transform', `2400px`);
+            
+        }
+    })
+
+
+
+})
---
+++ js/swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+   
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable : true,
+    },
+
+    autoplay: {
+        delay: 5000,
+      },
+
+      effect: 'fade',
+      speed: 2000,
+
+  });
